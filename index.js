3e75670c461fef223aa939ed9c3c28acffcc6c23
ahmedfekry5 <ahmed.fekry@ivas.com.eg>
2017-07-11 15:21:28 +0200

    fekry changing the port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -25,6 +25,6 @@ io.on('connection',function(socket) {
 
 });
 
-server.listen(8080, function() {
+server.listen(port, function() {
 	console.log('listening on *:8080');
 });
